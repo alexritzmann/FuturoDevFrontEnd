97284a6a7aa321be2cd5ca56b81422d58b374d87
alexritzmann <alexritzmann@gmail.com>
2025-07-22 09:37:34 -0300

    M02S08 - Introdução

---
+++ Trello/M02S08/meu-app-react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='container-header'>
+      <header id='header'>
+        <h1>Energia Solar 365</h1>
+        <h3>Economize com Sol. Invista no seu futuro!</h3>
+      </header>
+    </div>
+    </>
+  )
+}
+
+export default App
